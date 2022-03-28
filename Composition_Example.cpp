e52c262bfaed5811f64421cf8e1013ea6dfd1b39
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2022-03-28 11:55:22 +0530

    Add files via upload

---
+++ Composition_Example.cpp
@@ -0,0 +1,44 @@
+//Aggregation Relationship in C++
+
+#include <iostream>
+#include <string>
+
+class Techie
+{
+public:
+    Techie(int hn = 0, std::string st = "", std::string cn = "", std::string sn = "") :house_no(hn), state(st), city_name(cn), street_name(sn) {}
+    int house_no;
+    std::string state;
+    std::string city_name;
+    std::string street_name; 
+};
+
+class Person //Composition
+{
+public:
+    Person(std::string p_name, const Techie& addr)
+    {
+        name = p_name;
+        address = addr;
+    }
+
+    void display()
+    {
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "House No: " << address.house_no << std::endl;
+        std::cout << "State: " << address.city_name << std::endl;
+        std::cout << "Street: " << address.street_name << std::endl;    
+    }
+
+private:
+    std::string name;
+    Techie address;
+};
+
+int main()
+{
+    Techie technical_guy(131, "KARNATAKA", "BANGALORE", "SINGASANDRA");
+    Person person("Ramesh", technical_guy);
+    person.display();
+    return 0;
+}
